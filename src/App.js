adb6923f537b8594708cad6a5959da9ee205e95a
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2022-01-30 00:06:03 +0900

    add regex-mode

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ const db = firebase.firestore();
 
 const App = () => {
   // ステート郡
-  const [logs, setLogs] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState('rubyfuriMode')
+  // ルビ振り関連のステート
   const [ruby1, setRuby1] = useState('');
   const [ruby2, setRuby2] = useState('');
   const [ruby3, setRuby3] = useState('');
@@ -19,9 +21,17 @@ const App = () => {
   const [kanji, setKanji] = useState('');
   const [jukuji, setJukuji] = useState('');
   const [ruby_j, setRuby_j] = useState('');
+  const [logs, setLogs] = useState([]);
   const [edit, setEdit] = useState(false);
-  const [open, setOpen] = useState(false);
-  const [activeItem, setActiveItem] = useState('rubyfuri')
+  // 正規表現関連のステート
+  const [word1, setWord1] = useState('');
+  const [word2, setWord2] = useState('');
+  const [word3, setWord3] = useState('');
+  const [word4, setWord4] = useState('');
+  const [hiragana1, setHiragana1] = useState('');
+  const [hiragana2, setHiragana2] = useState('');
+  const [hiragana3, setHiragana3] = useState('');
+  const [hiragana4, setHiragana4] = useState('');
 
   // 更新のお知らせの管理と履歴表示の状態監視
   useEffect(() => {
@@ -205,10 +215,24 @@ const App = () => {
     setRuby_j('')
   }
 
+  const RegresetBtn = () => {
+    setWord1('')
+    setWord2('')
+    setWord3('')
+    setWord4('')
+    setHiragana1('')
+    setHiragana2('')
+    setHiragana3('')
+    setHiragana4('')
+  }
+
   // コピーボタンとその関数
   const copyToClipboard = async () => {
-    if (Rubyfuri) {
+    if (activeItem === 'rubyfuriMode') {
       const copyData = document.getElementsByClassName("rubyText")[0].innerText
+      await navigator.clipboard.writeText(copyData)
+    } else if (activeItem === 'regexMode') {
+      const copyData = document.getElementsByClassName("regText")[0].innerText
       await navigator.clipboard.writeText(copyData)
     }
   }
@@ -226,7 +250,7 @@ const App = () => {
     }
     return (
       <Popup
-        trigger={<Button onClick={()=> copyToClipboard()}>COPY</Button>}
+        trigger={<Button onClick={()=> copyToClipboard()}><Icon name='copy outline'/>COPY</Button>}
         content={message}
         on='click'
         style={{"opacity":0.8}}
@@ -269,7 +293,7 @@ const App = () => {
         <Popup
           trigger={<button
             className="deleteBtn"
-            onClick={() => setEdit(true)}>履歴を削除する</button>}
+            onClick={() => setEdit(true)}><Icon name='trash alternate outline'/>履歴を削除する</button>}
           content='履歴を削除できるモードです。'
           on='hover'
           style={{"opacity":0.8}}
@@ -406,167 +430,72 @@ const App = () => {
     }
   }
 
-  const Head = () => {
-    return (
-      <div className="title">
-        <Menu size='huge' inverted secondary>
-          <Menu.Item header><h1>Ruby furifuri 2</h1></Menu.Item>
-          <Menu.Item></Menu.Item>
-          <Menu.Item
-            name='ルビ'
-            active={activeItem === 'rubyfuri'}
-            onClick={()=> setActiveItem('rubyfuri')}
-          />
-          <Menu.Item
-            name='正規表現'
-            active={activeItem === 'regex'}
-            onClick={()=> setActiveItem('regex')}
-          />
-        </Menu>
-      </div>
-    )
-  }
+  // 正規表現の表示コンポーネント
+  const Regexfuri = () => {
+    let reg1 = ""
+    let reg2 = ""
+    let reg3 = ""
+    let reg4 = ""
 
-  const Contents = () => {
-    if (activeItem === 'rubyfuri') {
+    if (word1 && hiragana1) {
+      reg1 = `(${word1}|${hiragana1})`
+    } else if (word1 && !hiragana1){
+      reg1 = `${word1}`
+    }
+    if (word2 && hiragana2) {
+      reg2 = `(${word2}|${hiragana2})`
+    } else if (word2 && !hiragana2){
+      reg2 = `${word2}`
+    }
+    if (word3 && hiragana3) {
+      reg3 = `(${word3}|${hiragana3})`
+    } else if (word3 && !hiragana3){
+      reg3 = `${word3}`
+    } 
+    if (word4 && hiragana4) {
+      reg4 = `(${word4}|${hiragana4})`
+    } else if (word4 && !hiragana4){
+      reg4 = `${word4}`
+    }
+
+    if  (reg1&&reg2&&reg3&&reg4) {
       return (
-        <div className="contents">
-        <Segment>
-          <Header as='h2' color='grey'>
-            <Icon name='rocket'/>
-            <Header.Content>入力欄</Header.Content>
-          </Header>
-          <Grid columns={2} stackable divided textAlign='center'>
-            <Grid.Column>
-              <div className="form">
-                <input
-                  className="kanji"
-                  value={kanji}
-                  placeholder="漢字を入力"
-                  autoFocus="true"
-                  onChange={(e) => {setKanji(e.target.value)}}
-                ></input><br />
-                <input
-                  className="ruby"
-                  value={ruby1}
-                  placeholder="ルビ①"
-                  onChange={(e) => {setRuby1(e.target.value)}}
-                ></input>
-                <input
-                  className="ruby"
-                  value={ruby2}
-                  placeholder="ルビ②"
-                  onChange={(e) => {setRuby2(e.target.value)}}
-                ></input>
-                <input
-                  className="ruby"
-                  value={ruby3}
-                  placeholder="ルビ③"
-                  onChange={(e) => {setRuby3(e.target.value)}}
-                ></input>
-                <input
-                  className="ruby"
-                  value={ruby4}
-                  placeholder="ルビ④"
-                  onChange={(e) => {setRuby4(e.target.value)}}
-                ></input>
-                <input
-                  className="ruby"
-                  value={ruby5}
-                  placeholder="ルビ⑤"
-                  onChange={(e) => {setRuby5(e.target.value)}}
-                ></input>
-                <input
-                  className="ruby"
-                  value={ruby6}
-                  placeholder="ルビ⑥"
-                  onChange={(e) => {setRuby6(e.target.value)}}
-                ></input>
-                <input
-                  className="ruby"
-                  value={ruby7}
-                  placeholder="ルビ⑦"
-                  onChange={(e) => {setRuby7(e.target.value)}}
-                ></input>
-                <input
-                  className="ruby"
-                  value={ruby8}
-                  placeholder="ルビ⑧"
-                  onChange={(e) => {setRuby8(e.target.value)}}
-                ></input>
-              </div>
-            </Grid.Column>
-            <Grid.Column>
-              <div className="form">
-                <input
-                  className="jukuji"
-                  value={jukuji}
-                  placeholder="熟字訓を入力"
-                  onChange={(e) => {setJukuji(e.target.value)}}
-                ></input><br />
-                <input
-                  className="ruby_j"
-                  value={ruby_j}
-                  placeholder="ルビ"
-                  onChange={(e) => {setRuby_j(e.target.value)}}
-                ></input>
-              </div>              
-            </Grid.Column>
-          </Grid>
-          <div><Popup
-              trigger={<button className="resetBtn" onClick={resetBtn}>RESET</button>}
-              content='入力欄がリセットされると同時に履歴欄に追加されます。（すでにあるものは追加されません）'
-              on='hover'
-              style={{"opacity":0.8}}
-              inverted
-              position="bottom right"
-              hideOnScroll
-              wide
-            /></div>
-        </Segment>
-        
-        <Segment>
-          <Header as='h2'color='grey'>
-            <Icon name='code'/>
-            <Header.Content>ルビタグ表示欄</Header.Content>
-          </Header>
-          <Grid>
-            <Grid.Column>
-              <div className="rubyContent">
-                <Rubyfuri />
-              </div>
-            </Grid.Column>
-          </Grid>
-        </Segment>
-        
-        <Segment>
-          <Header as='h2'color='grey'>
-            <DeleteBtn/>
-            <Icon name='history'/>
-            <Header.Content>最近の履歴</Header.Content>
-          </Header>
-          <Grid>
-            <Grid.Column>
-              <div className="historyContent">
-                {logItems}
-              </div>
-            </Grid.Column>
-          </Grid>
-        </Segment>
-        <p style={{"text-align":"right"}}>ver 1.2.8</p>
-      </div>
+        <div>
+          <h3 className="regText">{"^"}{reg1}{reg2}{reg3}{reg4}{"$"}</h3>
+          <CopyBtn/>
+        </div>
       )
-    } else if (activeItem === 'regex') {
+    } else if (reg1&&reg2&&reg3) {
       return (
-        <div className="contents"></div>
+        <div>
+          <h3 className="regText">{"^"}{reg1}{reg2}{reg3}{"$"}</h3>
+          <CopyBtn/>
+        </div>
+      )
+    } else if (reg1&&reg2) {
+      return (
+        <div>
+          <h3 className="regText">{"^"}{reg1}{reg2}{"$"}</h3>
+          <CopyBtn/>
+        </div>
+      ) 
+    } else if (reg1) {
+      return (
+        <div>
+          <h3 className="regText">{"^"}{reg1}{"$"}</h3>
+          <CopyBtn/>
+        </div>
+      )
+    } else {
+      return (
+        <div></div>
       )
     }
   }
 
-  // 基本的なレンダー部分
-  return (
-    <div className="App">
-      {/* お知らせモーダル */}
+  // お知らせモーダル
+  const MessageModal = () => {
+    return (
       <Modal
         closeOnDimmerClick={false}
         basic
@@ -588,12 +517,287 @@ const App = () => {
           </Button>
         </Modal.Actions>
       </Modal>
-      {/* ヘッダーとメニュー */}
-      <Head/>
-      {/* コンテンツ */}
-      <Contents/>
-    </div>
-  );
+    )
+  }
+
+  // ヘッダー
+  const Head = () => {
+    return (
+      <div className="title">
+        <Menu inverted secondary>
+        <Menu.Header as='h1'>Ruby furifuri 2</Menu.Header>
+        </Menu>
+        <Menu inverted secondary>
+          <Menu.Item
+            name='ルビ'
+            active={activeItem === 'rubyfuriMode'}
+            onClick={()=> setActiveItem('rubyfuriMode')}
+          />
+          <Menu.Item
+            name='正規表現'
+            active={activeItem === 'regexMode'}
+            onClick={()=> setActiveItem('regexMode')}
+          />
+          <Menu.Item position='right'>ver 2.0.0</Menu.Item>
+        </Menu>
+      </div>
+    )
+  }
+
+  // 基本的なレンダー部分
+  if (activeItem === 'rubyfuriMode') {
+    return (
+      <div className="App">
+        <MessageModal/>
+        <Head/>
+
+        <div className="contents">
+          <Segment>
+            <Header as='h2' color='grey'>
+              <Icon name='rocket'/>
+              <Header.Content>入力欄</Header.Content>
+            </Header>
+            <Grid columns={2} stackable divided textAlign='center'>
+              <Grid.Column>
+                <div className="form">
+                  <input
+                    className="kanji"
+                    value={kanji}
+                    placeholder="漢字を入力"
+                    autoFocus="true"
+                    onChange={(e) => {setKanji(e.target.value)}}
+                  ></input><br />
+                  <input
+                    className="ruby"
+                    value={ruby1}
+                    placeholder="ルビ①"
+                    onChange={(e) => {setRuby1(e.target.value)}}
+                  ></input>
+                  <input
+                    className="ruby"
+                    value={ruby2}
+                    placeholder="ルビ②"
+                    onChange={(e) => {setRuby2(e.target.value)}}
+                  ></input>
+                  <input
+                    className="ruby"
+                    value={ruby3}
+                    placeholder="ルビ③"
+                    onChange={(e) => {setRuby3(e.target.value)}}
+                  ></input>
+                  <input
+                    className="ruby"
+                    value={ruby4}
+                    placeholder="ルビ④"
+                    onChange={(e) => {setRuby4(e.target.value)}}
+                  ></input>
+                  <input
+                    className="ruby"
+                    value={ruby5}
+                    placeholder="ルビ⑤"
+                    onChange={(e) => {setRuby5(e.target.value)}}
+                  ></input>
+                  <input
+                    className="ruby"
+                    value={ruby6}
+                    placeholder="ルビ⑥"
+                    onChange={(e) => {setRuby6(e.target.value)}}
+                  ></input>
+                  <input
+                    className="ruby"
+                    value={ruby7}
+                    placeholder="ルビ⑦"
+                    onChange={(e) => {setRuby7(e.target.value)}}
+                  ></input>
+                  <input
+                    className="ruby"
+                    value={ruby8}
+                    placeholder="ルビ⑧"
+                    onChange={(e) => {setRuby8(e.target.value)}}
+                  ></input>
+                </div>
+              </Grid.Column>
+              <Grid.Column>
+                <div className="form">
+                  <input
+                    className="jukuji"
+                    value={jukuji}
+                    placeholder="熟字訓を入力"
+                    onChange={(e) => {setJukuji(e.target.value)}}
+                  ></input><br />
+                  <input
+                    className="ruby_j"
+                    value={ruby_j}
+                    placeholder="ルビ"
+                    onChange={(e) => {setRuby_j(e.target.value)}}
+                  ></input>
+                </div>              
+              </Grid.Column>
+            </Grid>
+            <div><Popup
+                trigger={<button className="resetBtn" onClick={resetBtn}>RESET</button>}
+                content='入力欄がリセットされると同時に履歴欄に追加されます。（すでにあるものは追加されません）'
+                on='hover'
+                style={{"opacity":0.8}}
+                inverted
+                position="bottom right"
+                hideOnScroll
+                wide
+              /></div>
+          </Segment>
+          
+          <Segment>
+            <Header as='h2'color='grey'>
+              <Icon name='code'/>
+              <Header.Content>ルビタグ表示欄</Header.Content>
+            </Header>
+            <Grid>
+              <Grid.Column>
+                <div className="rubyContent">
+                  <Rubyfuri />
+                </div>
+              </Grid.Column>
+            </Grid>
+          </Segment>
+          
+          <Segment>
+            <Header as='h2'color='grey'>
+              <DeleteBtn/>
+              <Icon name='history'/>
+              <Header.Content>最近の履歴</Header.Content>
+            </Header>
+            <Grid>
+              <Grid.Column>
+                <div className="historyContent">
+                  {logItems}
+                </div>
+              </Grid.Column>
+            </Grid>
+          </Segment>
+        </div>
+      </div>
+      
+    )
+  } else if (activeItem === 'regexMode') {
+    return (
+      <div className="App">
+        <MessageModal/>        
+        <Head/>
+        
+        <div className="contents">
+          <Segment>
+            <Header as='h2' color='grey'>
+              <Icon name='rocket'/>
+              <Header.Content>入力欄</Header.Content>
+            </Header>
+            <Grid columns={4} divided>
+              <Grid.Row>
+                <Grid.Column>
+                  <h3>①</h3>
+                  <input
+                    className="regexInput"
+                    value={word1}
+                    placeholder="（例）村"
+                    autoFocus="true"
+                    onChange={(e) => {setWord1(e.target.value)}}
+                  ></input>
+                  <input
+                    className="regexInput"
+                    value={hiragana1}
+                    placeholder="（例）むら"
+                    onChange={(e) => {setHiragana1(e.target.value)}}
+                  ></input>
+                </Grid.Column>
+                <Grid.Column>
+                  <h3>②</h3>
+                  <input
+                    className="regexInput"
+                    value={word2}
+                    placeholder="（例）田"
+                    onChange={(e) => {setWord2(e.target.value)}}
+                  ></input>
+                  <input
+                    className="regexInput"
+                    value={hiragana2}
+                    placeholder="（例）た"
+                    onChange={(e) => {setHiragana2(e.target.value)}}
+                  ></input>
+                </Grid.Column>
+                <Grid.Column>
+                  <h3>③</h3>
+                  <input
+                    className="regexInput"
+                    value={word3}
+                    placeholder="（例）さん"
+                    onChange={(e) => {setWord3(e.target.value)}}
+                  ></input>
+                  <input
+                    className="regexInput"
+                    value={hiragana3}
+                    placeholder=""
+                    onChange={(e) => {setHiragana3(e.target.value)}}
+                  ></input>
+                </Grid.Column>
+                <Grid.Column>
+                  <h3>④</h3>
+                  <input
+                    className="regexInput"
+                    value={word4}
+                    placeholder=""
+                    onChange={(e) => {setWord4(e.target.value)}}
+                  ></input>
+                  <input
+                    className="regexInput"
+                    value={hiragana4}
+                    placeholder=""
+                    onChange={(e) => {setHiragana4(e.target.value)}}
+                  ></input>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+            <div><Popup
+                trigger={<button className="resetBtn" onClick={RegresetBtn}>RESET</button>}
+                content='入力欄がリセットされます。（履歴追加は未対応）'
+                on='hover'
+                style={{"opacity":0.8}}
+                inverted
+                position="bottom right"
+                hideOnScroll
+                wide
+              /></div>
+          </Segment>
+
+          <Segment>
+            <Header as='h2'color='grey'>
+              <Icon name='code'/>
+              <Header.Content>正規表現表示欄</Header.Content>
+            </Header>
+            <br/>
+            <Grid>
+              <Grid.Column>
+                <div className="rubyContent">
+                  <Regexfuri/>
+                </div>
+              </Grid.Column>
+            </Grid>
+          </Segment>
+
+          <Segment>
+            <Header as='h2'color='grey'>
+              <Icon name='key'/>
+              <Header.Content>使い方</Header.Content>
+            </Header>
+            <Grid>
+              <Grid.Column>
+                <Icon name='star'/>
+                例えば「村田さん」の正規表現を作りたい場合は、①の上部に「村」、下部に「むら」のように入力していきます。<br/>　「さん」のように正規表現不要なものは、上部にだけ入力してください。
+              </Grid.Column>
+            </Grid>
+          </Segment>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
